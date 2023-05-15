fd4d214d613bd2281fe12412f6f86bdb149ee59f
Ľubomír Brindza <lubomir.brindza@websupport.sk>
2023-05-15 13:23:48 +0200

    fix setup.cfg, subclassing seems to crash Python (oops)

--- src/pymysqlite.py
+++ src/pymysqlite.py
@@ -7,60 +7,73 @@ from warnings import warn
 
 def _translate(sql: str) -> str:
     # replace placeholders
-    sql = re.sub(r"\b%[a-z]\b", "?", sql)
-    sql = re.sub(r"\b%\(([a-z]+)\)[a-z]\b", r":\1", sql)
+    sql = re.sub(r"%[a-z]\b", "?", sql)
+    sql = re.sub(r"%\(([a-z]+)\)[a-z]\b", r":\1", sql)
     return sql
 
 
-class Connection(sqlite3.Connection):
+class Connection:
     def __init__(self, database: str, *args, **kwargs) -> None:
         self._connection = sqlite3.connect(database, *args, **kwargs)
 
     def cursor(self):
-        return Cursor(self)
+        _cursor = self._connection.cursor()
+        return Cursor(_cursor)
 
     def close(self) -> None:
-        return super().close()
+        self._connection.close()
+
+    def commit(self) -> None:
+        self._connection.commit()
 
     # While pymysql disallows calling execute* methods from the Connection
     # object directly, these can still be useful in the test set up stage.
 
+    def executescript(self, script: str):
+        self._connection.executescript(script)
+
     def execute(self, sql: str, parameters):  # type:ignore
         warn("pymysql does not allow calling execute() directly from the connection object")
-        return super().execute(_translate(sql), parameters)
+        return self._connection.execute(_translate(sql), parameters)
 
     def executemany(self, sql: str, parameters):
         warn("pymysql does not allow calling executemany() directly from the connection object")
-        return super().executemany(_translate(sql), parameters)
+        return self._connection.executemany(_translate(sql), parameters)
 
 
-class Cursor(sqlite3.Cursor):
-    def __init__(self, conn) -> None:
-        self._last_stmt: str | None = None
-        super().__init__(conn)
+class Cursor:
+    def __init__(self, _cursor) -> None:
+        self._cursor = _cursor
 
-    def execute(self, sql: str, parameters) -> int:  # type:ignore
+    def execute(self, sql: str, args=None) -> int:  # type:ignore
         # FIXME: cursor.rowcount for SELECT statements is always -1 (which evals as True)
         sql = _translate(sql)
-        self._last_stmt = sql
-        super().execute(sql, parameters)
+        self._cursor.execute(sql, args or [])
         if not sql.startswith("SELECT"):
-            return self.rowcount
+            return self._cursor.rowcount
         warn("SELECT statements always return -1, do not depend on this value in your code")
         return -1
 
-    def executemany(self, sql: str, parameters) -> int:  # type:ignore
+    def executemany(self, sql: str, args) -> int:  # type:ignore
         sql = _translate(sql)
-        self._last_stmt = sql
-        super().executemany(sql, parameters)
-        return self.rowcount
+        self._cursor.executemany(sql, args)
+        return self._cursor.rowcount
+
+    def fetchone(self):
+        return self._cursor.fetchone()
+
+    def fetchmany(self):
+        return self._cursor.fetchmany()
+
+    def fetchall(self):
+        return self._cursor.fetchall()
 
     def __enter__(self):
         return self
 
     def __exit__(self, *exc_info):
         del exc_info
-        self.close()
+        self._cursor.close()
 
 
 def connect(database: str) -> Connection:
