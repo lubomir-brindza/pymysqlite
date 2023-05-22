d528208a419a0b2869612f7d07940c11ddaaa607
Ľubomír Brindza <lubomir.brindza@websupport.sk>
2023-05-22 13:52:31 +0200

    [0.0.3] add/enable custom converters for datetime objects

--- build/lib/pymysqlite.py
+++ build/lib/pymysqlite.py
@@ -14,12 +14,8 @@ def _translate(sql: str) -> str:
 
 
 class Connection:
-    def __init__(self, database: str, **kwargs) -> None:
-        self._connection = sqlite3.connect(
-            database,
-            detect_types=sqlite3.PARSE_DECLTYPES,
-            **kwargs,
-        )
+    def __init__(self, database: str, *args, **kwargs) -> None:
+        self._connection = sqlite3.connect(database, *args, **kwargs)
 
     def cursor(self):
         _cursor = self._connection.cursor()
@@ -84,19 +80,43 @@ class Cursor:
 def connect(database: str) -> Connection:
     return Connection(database)
 
-# custom type converters
-# - datetime (date and timestamp already have implicit converters)
+# non-native type converters
 
 
-def adapt_datetime_iso(val: datetime.datetime) -> str:
+def adapt_date_iso(val):
+    """Adapt datetime.date to ISO 8601 date."""
+    return val.isoformat()
+
+
+def adapt_datetime_iso(val):
     """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
     return val.isoformat()
 
 
-def convert_datetime(val: bytes) -> datetime.datetime:
+def adapt_datetime_epoch(val):
+    """Adapt datetime.datetime to Unix timestamp."""
+    return int(val.timestamp())
+
+
+def convert_date(val):
+    """Convert ISO 8601 date to datetime.date object."""
+    return datetime.date.fromisoformat(val.decode())
+
+
+def convert_datetime(val):
     """Convert ISO 8601 datetime to datetime.datetime object."""
     return datetime.datetime.fromisoformat(val.decode())
 
 
+def convert_timestamp(val):
+    """Convert Unix epoch timestamp to datetime.datetime object."""
+    return datetime.datetime.fromtimestamp(int(val))
+
+
+sqlite3.register_adapter(datetime.date, adapt_date_iso)
 sqlite3.register_adapter(datetime.datetime, adapt_datetime_iso)
+sqlite3.register_adapter(datetime.datetime, adapt_datetime_epoch)
+
+sqlite3.register_converter("date", convert_date)
 sqlite3.register_converter("datetime", convert_datetime)
+sqlite3.register_converter("timestamp", convert_timestamp)
